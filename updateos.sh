d146a1e9ad578b05f023e237e2602803225bef8c
lbaitemple <lbai@temple.edu>
2021-03-05 16:35:19 -0500

    Update updateos.sh

--- updateos.sh
+++ updateos.sh
@@ -6,8 +6,8 @@ sudo pip3 install --upgrade numpy
 
 # install tensorflow
 sudo apt-get update
-sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
-sudo apt-get install python3-pip
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran -y
+sudo apt-get install python3-pip -y
 sudo pip3 install -U pip testresources setuptools==49.6.0 
 sudo pip3 install -U numpy==1.19.4 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
@@ -45,7 +45,7 @@ cd
 sudo apt install python3-smbus
 git clone https://github.com/lbaitemple/jetbot
 cd jetbot
-sudo apt-get install cmake
+sudo apt-get install cmake -y
 sudo python3 setup.py install
 
 cd jetbot/utils
